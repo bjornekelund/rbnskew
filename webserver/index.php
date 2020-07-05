d64c5f45c701d76c8cb6cb8855996f3b8b1090fc
Björn Ekelund <bjorn@ekelund.nu>
2020-07-05 16:27:16 +0200

    Update

--- webserver/index.php
+++ webserver/index.php
@@ -5,12 +5,12 @@
 </head>
 <body>
 
-<h1>Welcome to SM7IUN RBN Analytics</h1>
+<h1>Welcome to SM7IUN RBN Skew Analytics</h1>
 <?php
 #    echo "Current time: ";
 #    echo date('Y-m-d H:i:s');
     echo "<pre>";
-    echo file_get_contents("/home/pi/rbnskew/rbnskew.txt");
+    echo file_get_contents("/home/pi/rbnskew/webserver/rbnskew.txt");
     echo "</pre>";
 ?>
 </body>
