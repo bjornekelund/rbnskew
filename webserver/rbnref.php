5332849254d3c1e03ea82c3ebae1bceb1536d2e2
Bjorn Ekelund <bjorn@ekelund.nu>
2020-07-08 09:53:25 +0200

    Reference integrity web page

---
+++ webserver/rbnref.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>SM7IUN</title>
+</head>
+<body>
+
+<h1>RBN Skew Analytics</h1>
+<?php
+    echo "<pre>";
+    echo file_get_contents("rbnref.txt");
+    echo "</pre>";
+?>
+</body>
+</html>
---
+++ webserver/rbnskewr.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>SM7IUN</title>
+</head>
+<body>
+
+<h1>RBN Skew Analytics - RTTY</h1>
+<?php
+    echo "<pre>";
+    echo file_get_contents("rbnskewr.txt");
+    echo "</pre>";
+?>
+</body>
+</html>
